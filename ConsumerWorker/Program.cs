a88473c8309ae61f1736cfb732e27e4a330eddb7
reyhanakcathomson <reyhan.akca@thomsonreuters.com>
2024-11-24 22:05:23 +0300

    deleted the consumer moved codes

--- ConsumerWorker/Program.cs
+++ ConsumerWorker/Program.cs
@@ -1,7 +1,6 @@
 using ConsumerWorker;
-using ConsumerWorker.Consumers;
+using Infrastructure.Extensions;
 using Infrastructure.MessageBroker;
-using MassTransit;
 using Microsoft.Extensions.Options;
 
 var builder = Host.CreateDefaultBuilder(args);
@@ -17,32 +16,7 @@ builder.ConfigureServices((hostContext, services) =>
 
     services.AddSingleton(sp => sp.GetRequiredService<IOptions<MessageBrokerSettings>>().Value);
 
-    services.AddMassTransit(configure =>
-    {
-        configure.AddConsumer<LedgerDataUploadedSendEmailConsumer>();
-        configure.AddConsumer<LedgerDataUploadedLogConsumer>();
-        configure.AddConsumer<LedgerDataAnalyzeConsumer>();
-
-        configure.SetKebabCaseEndpointNameFormatter();
-
-        configure.UsingRabbitMq((context, cfg) =>
-        {
-            MessageBrokerSettings settings = context.GetRequiredService<MessageBrokerSettings>();
-            cfg.Host(new Uri(settings.Host),h =>
-            {
-                h.Username(settings.Username);
-                h.Password(settings.Password);
-            });
-
-            cfg.ReceiveEndpoint("ledger-data-uploaded-log",
-                e => { e.ConfigureConsumer<LedgerDataUploadedLogConsumer>(context); });
-            cfg.ReceiveEndpoint("ledger-data-uploaded-send-email",
-                e => { e.ConfigureConsumer<LedgerDataUploadedSendEmailConsumer>(context); });
-            cfg.ReceiveEndpoint("ledger-data-analyze",
-                e => { e.ConfigureConsumer<LedgerDataAnalyzeConsumer>(context); });
-
-        });
-    });
+    services.AddCustomMassTransit();
 
     services.AddHostedService<ConsumerService>();
 });
